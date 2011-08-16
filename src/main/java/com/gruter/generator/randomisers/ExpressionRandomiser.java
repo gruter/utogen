68af3c77e21d3570c7d6e6fcb66b07d1e279aad8
hyungjoonkim <babokim@gmail.com>
2011-08-16 13:54:21 +0900

    add ExpressionRandomiser

---
+++ src/main/java/com/gruter/generator/randomisers/ExpressionRandomiser.java
@@ -0,0 +1,19 @@
+package com.gruter.generator.randomisers;
+
+import generator.extenders.IRandomiserFunctionality;
+import generator.extenders.RandomiserInstance;
+
+public class ExpressionRandomiser implements IRandomiserFunctionality {
+  @Override
+  public void destroy() {
+  }
+
+  @Override
+  public Object generate() {
+    return "";
+  }
+
+  @Override
+  public void setRandomiserInstance(RandomiserInstance ri) {
+  }
+}
